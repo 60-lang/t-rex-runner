d183d7700f15442c75c7242d868112c572eb38c4
60-lang <qkrwnsgh60@gmail.com>
2025-11-04 10:23:10 +0900

    Adjust game physics constants for Trex config

--- index.js
+++ index.js
@@ -1561,12 +1561,12 @@
      */
     Trex.config = {
         DROP_VELOCITY: -5,
-        GRAVITY: 0.6,
-        HEIGHT: 47,
+        GRAVITY: 0.1,
+        HEIGHT: 10,
         HEIGHT_DUCK: 25,
         INIITAL_JUMP_VELOCITY: -10,
         INTRO_DURATION: 1500,
-        MAX_JUMP_HEIGHT: 30,
+        MAX_JUMP_HEIGHT: 3000,
         MIN_JUMP_HEIGHT: 30,
         SPEED_DROP_COEFFICIENT: 3,
         SPRITE_WIDTH: 262,
